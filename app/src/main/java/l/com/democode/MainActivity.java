e4d1c28082f31c9ce2eb605dfd10c0b13bf95abd
Anurag.Mishra <anurag.mishra@srmtecsol.com>
2018-03-30 10:03:33 +0530

    Tish for test purpose

---
+++ app/src/main/java/l/com/democode/MainActivity.java
@@ -0,0 +1,293 @@
+package l.com.democode;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.QuickContactBadge;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int SELECT_PICTURE = 100;
+    private static final String TAG = "MainActivity";
+    Button btn_attach;
+    HttpEntity resEntity;
+    ImageView imageView;
+    Bitmap bitmap1;
+    TextView messageText;
+    Button uploadButton;
+    int serverResponseCode = 0;
+    ProgressDialog dialog = null;
+
+    String upLoadServerUri = null;
+
+    /**********  File Path *************/
+    String uploadFilePath;
+    String uploadFileName = "service_lifecycle.png";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        btn_attach = (Button) findViewById(R.id.button);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openImageChooser();
+            }
+        });
+
+        btn_attach.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(bitmap1!= null) {
+                    new HttpAsyncTask().execute("http://sales.namasteled.com/index.php/ws/uploadimage");
+                }else{
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    
+    /* Choose an image from Gallery */
+    void openImageChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // Get the path from the Uri
+                        try {
+                            Bitmap bitmap = decodeBitmapUri(this, selectedImageUri);
+                            bitmap1 = getBitmapFromUri(selectedImageUri);
+                            imageView.setImageBitmap(bitmap1);
+                            System.out.println("------selectedUri-----//" + bitmap1);
+                        }catch (IOException e) {
+
+                        }
+                    uploadFilePath = getPathFromURI(selectedImageUri);
+                        Log.i(TAG, "Image Path : " + uploadFilePath);
+                        // Set the image in ImageView
+                        imageView.setImageURI(selectedImageUri);
+                }
+            }
+        }
+    }
+
+    private Bitmap decodeBitmapUri(Context ctx, Uri uri) throws FileNotFoundException {
+        int targetW = 600;
+        int targetH = 600;
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(ctx.getContentResolver().openInputStream(uri), null, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+
+        return BitmapFactory.decodeStream(ctx.getContentResolver()
+                .openInputStream(uri), null, bmOptions);
+    }
+
+    /* Get the real path from the URI */
+    public String getPathFromURI(Uri contentUri){
+        String res = null;
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
+        if (cursor.moveToFirst()) {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            res = cursor.getString(column_index);
+        }
+        cursor.close();
+        return res;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            System.out.println("-------url---//" + urls[0].toString());
+            return POST(urls[0], bitmap1);
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject obj = new JSONObject(result);
+                String Status = obj.getString("result");
+                String response = obj.getString("responseMsg");
+                if (Status.equalsIgnoreCase("1")) {
+                    try {
+                        Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+                        finish();
+                    } catch (Exception e) {
+
+                    }
+                } else if (Status.equalsIgnoreCase("false")) {
+                }else if (Status.equalsIgnoreCase("")){
+                    finish();
+                }
+            } catch (Throwable t) {
+
+            }
+        }
+    }
+
+    // for send register data to server with post method
+    public String POST(String url, Bitmap bitmap) {
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // 1. create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // 2. make POST request to the given URL
+            HttpPost httpPost = new HttpPost(url);
+
+            MultipartEntity reqEntity = new MultipartEntity();
+            String json = "";
+            // 3. build jsonObject
+            JSONObject jsonObject = new JSONObject();
+
+            if (bitmap != null) {
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 75, bos);
+                byte[] data = bos.toByteArray();
+                String encodedString = Base64.encodeToString(data, Base64.DEFAULT);
+                System.out.println("----------Baba Value-----//"+encodedString);
+                reqEntity.addPart("file", new StringBody(encodedString));
+            }
+
+            httpPost.setEntity(reqEntity);
+            HttpResponse response = httpclient.execute(httpPost);
+            resEntity = response.getEntity();
+            final String response_str = EntityUtils.toString(resEntity);
+            if (resEntity != null) {
+                Log.i("RESPONSE", response_str);
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        try {
+                            System.out.println("Response: " + response_str);
+                            JSONObject obj = new JSONObject(response_str);
+                            String Status = obj.getString("result");
+                            String message = obj.getString("responseMsg");
+                            try {
+                                finish();
+                            } catch (Exception e) {
+                                Log.d("InputStream", e.getLocalizedMessage());
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            System.out.println("error-----" + e);
+                        }
+                    }
+                });
+            }
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+        return result;
+    }
+
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while ((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
+}
